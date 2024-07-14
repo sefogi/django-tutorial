ca4aa9af9e0cc7296e5a66ebf59c8eaf50c17ba3
sefogi <sebast18@gmail.com>
2024-07-14 18:52:20 +0200

    tutorial actualizado

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.hello ),
+    
+    path('', views.index ),
     path('about/',views.about),
-    path('gallery/', views.gallery)
+    path('gallery/', views.gallery),
+    path('hello/<str:username>', views.hello ),
     
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -2,8 +2,11 @@
 from django.http import HttpResponse
 
 # Create your views here.
-def hello(request):
-    return HttpResponse("<h2>Hello world</h2>")
+def index(request):
+    return HttpResponse("<h2>index page.</h2>")
+def hello(request, username):
+    print(username)
+    return HttpResponse("<h2>Hello world {}</h2>".format(username) )
 
 def about(request):
     return HttpResponse("<h1>estas viendo la seccion About</h1>")
