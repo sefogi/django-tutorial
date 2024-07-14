9a60606a7a1b3f6c6a4e63c8be49a9417f8c3b4c
sefogi <sebast18@gmail.com>
2024-07-14 15:25:29 +0200

    creacion de modelos bd

--- myapp/views.py
+++ myapp/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def hello(request):
-    return HttpResponse("<h1>Hello world</h1>")
+    return HttpResponse("<h2>Hello world</h2>")
 
 def about(request):
     return HttpResponse("<h1>estas viendo la seccion About</h1>")
